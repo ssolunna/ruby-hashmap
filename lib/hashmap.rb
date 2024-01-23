7d41232bb3a9fc011c0870d5ae1927cda4a98739
ssolunna <ssolunna29@gmail.com>
2024-01-23 10:59:05 -0500

    Add hash function

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class HashMap
+  def initialize(initial_size = 16)
+    @buckets = Array.new(initial_size)
+  end
+
+  def hash(key) # key must be of type String
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code % @buckets.length
+  end
 end
--- spec/hashmap_spec.rb
+++ spec/hashmap_spec.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
 
 require_relative '../lib/hashmap'
+
+describe HashMap do
+  describe '#hash' do
+    subject(:hashmap) { described_class.new }
+
+    it 'returns the hash code of a given string (key)' do
+      hash_code = 9
+      key = 'Manon'
+      result = hashmap.hash(key)
+      expect(result).to eq(hash_code)
+    end
+  end
+end
